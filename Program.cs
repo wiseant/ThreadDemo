6ed6bdb4dc0791a9ea66497d50ce8aa3ff7b9014
wiseant <wiseant@yeah.net>
2019-06-20 11:02:57 +0800

    1.多线程中使用单例模式

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunMultiThreadTest();
+        }
+
+        static void RunMultiThreadTest()
+        {
+            while (true)
+            {
+                Console.WriteLine("1.正例一\n2.正例二\n3.反例\n请选择：");
+                var input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        TestAccurately();
+                        return;
+                    case "2":
+                        TestAccurately();
+                        return;
+                    case "3":
+                        TestFaultily();
+                        return;
+                    default:
+                        Console.WriteLine("请输入正确的序号！");
+                        break;
+                }
+            }
+        }
+
+        private static void TestAccurately()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                new Thread(new ThreadStart(Test)).Start();
+            }
+
+            void Test()
+            {
+                Console.WriteLine($"{Singleton.Instance.Id}");
+            }
+        }
+
+        private static void TestAccurately2()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                new Thread(new ThreadStart(Test)).Start();
+            }
+
+            void Test()
+            {
+                Console.WriteLine($"{SingletonToo.Instance.Id}");
+            }
+        }
+
+        private static void TestFaultily()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                new Thread(new ThreadStart(Test)).Start();
+            }
+
+            void Test()
+            {
+                Console.WriteLine($"{SingletonFaulty.Instance.Id}");
+            }
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThreadDemo
+{
+    public class Singleton
+    {
+        public Guid Id { get; }
+        private static Singleton instance;
+        //使用线程锁，在创建实例前先加锁，可以保证线程安全
+        private static readonly object obj_lock = new object();
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (obj_lock)
+                    {
+                        //线程锁定后一定要再次检查instance是否为null。
+                        if (instance == null)
+                            instance = new Singleton();
+                    }
+                }
+                return instance;
+            }
+        }
+
+        private Singleton()
+        {
+            this.Id = Guid.NewGuid();
+            System.Threading.Thread.Sleep(5 * 1000);//等待5秒
+            Console.WriteLine($"{this.GetType().Name} created, Id={Id}");
+        }
+    }
+
+    public class SingletonToo
+    {
+        public Guid Id { get; }
+        //使用饿汉模式，在声明静态属性时创建实例也是线程安全的。
+        private static readonly SingletonToo instance = new SingletonToo();
+
+        public static SingletonToo Instance => instance;
+
+        private SingletonToo()
+        {
+            this.Id = Guid.NewGuid();
+            System.Threading.Thread.Sleep(5 * 1000);//等待5秒
+            Console.WriteLine($"{this.GetType().Name} created, Id={Id}");
+        }
+    }
+
+    public class SingletonFaulty
+    {
+        public Guid Id { get; }
+        private static SingletonFaulty instance;
+        
+        // 这种方式获取单个实例是非线程安全的
+        public static SingletonFaulty Instance => instance ?? (instance = new SingletonFaulty());
+
+
+        private SingletonFaulty()
+        {
+            this.Id = Guid.NewGuid();
+            System.Threading.Thread.Sleep(5 * 1000);//等待5秒
+            Console.WriteLine($"{this.GetType().Name} created, Id={Id}");
+        }
+    }
+}
